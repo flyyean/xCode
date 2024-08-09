b58a48da1f6f658f183653711cc2c054f1238502
Alikhan <98089808+flyyean@users.noreply.github.com>
2024-08-09 10:36:26 +0500

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,7 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-print("Salem, alem")
+print("Я сегодня изучил:")
+print("Основы языка Swift")
+print("Как использовать Playgrounds")
+print("Любимая песна: Linkin Park - In the end")
